650fc406120929c174c97eeb5d5a3fbb2b611382
Akezhan Emperor <akejanmuzamelov@gmail.com>
2026-01-12 17:12:40 +0500

    Yeeeee

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import edu.aitu.oop3.db.DatabaseConnection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public class Main {
+    void main() {
+        System.out.println("Connecting to Supabase...");
+        try (Connection connection = DatabaseConnection.getConnection()){
+            System.out.println("Connection successful!");
+
+            String sql = "SELECT CURRENT_TIMESTAMP";
+            try (PreparedStatement stmt = connection.prepareStatement(sql);
+                 ResultSet rs = stmt.executeQuery()){
+
+                if(rs.next()){
+                    System.out.println("Database time:" + rs.getTimestamp(1));
+                }
+            }
+        }catch (SQLException e){
+            System.out.println("Error while connecting to database:");
+            e.printStackTrace();
+        }
+    }
+}
