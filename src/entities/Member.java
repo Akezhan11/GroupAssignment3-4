17c26e7ec5fc95ac0d160e2b35f79c657214dfa1
Akezhan Emperor <akejanmuzamelov@gmail.com>
2026-01-12 22:11:00 +0500

    at least i tried to add some getters and setters

--- src/entities/Member.java
+++ src/entities/Member.java
@@ -1,4 +1,91 @@
 package entities;
 
 public class Member {
+    private int id;
+    private static int idGen;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String gender;
+    private String email;
+
+    public Member(String name, String surname, String phoneNumber, String gender, String email) {
+        this.id = idGen++;
+        setName(name);
+        setSurname(surname);
+        setPhoneNumber(phoneNumber);
+        setGender(gender);
+        setEmail(email);
+    }
+
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        if(name == null||name.length() <= 1){
+            throw new IllegalArgumentException("Name must be at least 2 characters long!");
+        }
+        this.name = name;
+    }
+
+
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        if(surname == null||surname.length() <= 1){
+            throw new IllegalArgumentException("Surname must be at least 2 characters long!");
+        }
+        this.surname = surname;
+    }
+
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public static boolean isValidPhone(String phone) {
+        if (phone == null) return false;
+        return phone.matches("\\+?\\d{10,13}");
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        if(!isValidPhone(phoneNumber)){
+            throw new IllegalArgumentException("Invalid phone number!");
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getGender(){
+        return gender;
+    }
+    public void setGender(String gender){
+        boolean male = gender.toLowerCase().equals("male");
+        boolean female = gender.toLowerCase().equals("female");
+        if(!(male || female)){
+            throw new IllegalArgumentException("Invalid gender!");
+        }
+        this.gender = gender;
+    }
+
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    @Override
+    public String toString(){
+        return "id: " + id + ", name: " + name + ", surname: " + surname + ", phone: " + phoneNumber + ", gender: " + gender + ", email: " + email;
+    }
 }
